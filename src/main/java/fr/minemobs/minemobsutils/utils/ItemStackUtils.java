7aa30a0ca8a0d537a8934ad3de9af515ec898e4c
NoaLeGeek68 <62206118+NoaLeGeek68@users.noreply.github.com>
2021-07-14 13:47:00 +0200

    Some things
    list of banners/skulls and functions randomskull/randombanner

--- src/main/java/fr/minemobs/minemobsutils/utils/ItemStackUtils.java
+++ src/main/java/fr/minemobs/minemobsutils/utils/ItemStackUtils.java
@@ -18,7 +18,10 @@ public class ItemStackUtils {
     Material.GOLDEN_HELMET, Material.GOLDEN_CHESTPLATE, Material.GOLDEN_LEGGINGS, Material.GOLDEN_BOOTS,
     Material.CHAINMAIL_HELMET, Material.CHAINMAIL_CHESTPLATE, Material.CHAINMAIL_LEGGINGS, Material.CHAINMAIL_BOOTS,
     Material.LEATHER_HELMET, Material.LEATHER_CHESTPLATE, Material.LEATHER_LEGGINGS, Material.LEATHER_BOOTS};
-
+    private static final Material[] banners = new Material[]{Material.WHITE_BANNER, Material.ORANGE_BANNER, Material.MAGENTA_BANNER, Material.LIGHT_BLUE_BANNER, Material.YELLOW_BANNER, Material.LIME_BANNER,
+            Material.PINK_BANNER, Material.GRAY_BANNER, Material.LIGHT_GRAY_BANNER, Material.CYAN_BANNER, Material.PURPLE_BANNER, Material.BLUE_BANNER, Material.BROWN_BANNER, Material.GREEN_BANNER, Material.RED_BANNER,
+            Material.BLACK_BANNER};
+    private static final Material[] skulls = new Material[]{Material.CREEPER_HEAD, Material.DRAGON_HEAD, Material.PLAYER_HEAD, Material.ZOMBIE_HEAD, Material.WITHER_SKELETON_SKULL, Material.SKELETON_SKULL};
 
     public static boolean isAnArmor(ItemStack is) {
         return Arrays.stream(armors).anyMatch(material -> material == is.getType());
@@ -37,5 +40,13 @@ public class ItemStackUtils {
 
     public static boolean isAirOrNull(ItemStack item){
         return item == null || item.getType().isAir();
+    }
+    
+    public static Material randomBanner(){
+        return banners[new Random().nextInt(banners.length)];
+    }
+
+    public static Material randomSkull(){
+        return skulls[new Random().nextInt(skulls.length)];
     }
 }
